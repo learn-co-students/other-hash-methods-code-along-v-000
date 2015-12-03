49919654107bd33914f3f0808c82561797471492
ed-karabinus <ejkarabin@gmail.com>
2015-12-03 02:09:59 -0600

    Finished Other Hashes code-along; added definitions to various methods in lib/keys.rb, lib/min.rb, and lib/values.rb. Also added definition of hash groceries to bin/keys, bin/min, and bin/values.

--- lib/values.rb
+++ lib/values.rb
@@ -1,13 +1,14 @@
 require 'pry'
 
-#This is the groceries hash we'll be passing in to the get_the_values method
-# groceries = {
-#  dairy: ["milk", "yogurt", "cheese"],
-#  vegetable: ["carrots", "broccoli", "cucumbers"],
-#  meat: ["chicken", "steak", "salmon"],
-#  grains: ["rice", "pasta"]
-# }
+# This is the groceries hash we'll be passing in to the get_the_values method
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
 
-def get_the_values(groceries)
+def get_the_values(grocery_hash)
   #code your solution here!
+  grocery_hash.values.flatten
 end
