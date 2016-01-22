35a2e911b8c6b9e992a8362bf45ccb4647be1e76
Kevin McCormack <wittyman37@yahoo.com>
2016-01-22 16:59:26 -0500

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -10,4 +10,10 @@ require 'pry'
 
 def get_the_min(groceries)
   #code your solution here!
+  groceries.values.flatten.min
+  #groceries.each { |k,items|
+  #  items.each { |item|
+  #
+  #    }
+  #  }
 end
