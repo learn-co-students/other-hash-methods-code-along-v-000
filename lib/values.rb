6ea0693f9ea6a3d12b0bd94609666a3fe1e02834
Nicholas-Maxwell <nick.maxwell18@gmail.com>
2016-06-03 12:08:16 -0400

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -1,4 +1,4 @@
-require 'pry'
+#require 'pry'
 
 #This is the groceries hash we'll be passing in to the get_the_values method
 # groceries = {
@@ -9,5 +9,20 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+#groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
+
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+ }
+
+groceries.values.flatten
+
+#.Values COLLECTS all of the values in a hash & returns an array of the values of the keys in the hash
+#==> "milk", "yogurt", "cheese"], ["carrots", "broccoli", "cucumbers"], ["chicken", "steak", "salmon"], ["rice", "pasta"
+#Flatten --> get all the values in a Flat Array. 
+#binding.pry
+
 end
