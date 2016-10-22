175282436a6a9ac513f7a06e6ae5abaf2e33e1b9
Nicholas Ratti <nickratti@gmail.com>
2016-10-22 20:15:54 +0000

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -9,5 +9,14 @@ require 'pry'
 # }
 
 def get_the_min(groceries)
-  #code your solution here!
+  new_array = []
+  groceries.each do |food_group, item_hash|
+    item_hash.each do |food|
+      #binding.pry
+      new_array << food
+    end
+  end
+  new_array.sort
+  new_array.sort[0]
+  #binding.pry
 end
