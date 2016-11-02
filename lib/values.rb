2fa77d0ec63afda069d28a19b0c7b680aecad90f
Keegan Leitz <kjleitz@gmail.com>
2016-11-02 18:46:34 +0000

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -9,5 +9,10 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+  # This works! But I didn't read ahead and realize there's a #flatten method...
+  # groceries.values.inject do |grocery_list, ingredient_array|
+  #   ingredient_array.each {|ingredient| grocery_list << ingredient}
+  #   grocery_list
+  # end
+  groceries.values.flatten
 end
