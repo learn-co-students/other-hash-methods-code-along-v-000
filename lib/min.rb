2c83f15aa55f4716c4de072d7bfebcb2252c08ce
Jingru Zhang <zhang.jingru.93@gmail.com>
2017-03-01 20:07:37 +0000

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -9,5 +9,12 @@ require 'pry'
 # }
 
 def get_the_min(groceries)
-  #code your solution here!
+  groceries = {
+    dairy: ["milk", "yogurt", "cheese"],
+    vegetable: ["carrots", "broccoli", "cucumbers"],
+    meat: ["chicken", "steak", "salmon"],
+    grains: ["rice", "pasta"]
+  }
+  all_values = groceries.values.flatten
+  all_values.min
 end
