e2337288331d03230e9b30286f597d18eaac4f96
sarjumulmi <mulmi.sarju@gmail.com>
2017-04-13 12:56:09 -0400

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -7,7 +7,21 @@ require 'pry'
 #  meat: ["chicken", "steak", "salmon"],
 #  grains: ["rice", "pasta"]
 # }
-
+contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
 def get_the_values(groceries)
   #code your solution here!
+  groceries.values.flatten
+
 end
+puts get_the_values(contacts)
