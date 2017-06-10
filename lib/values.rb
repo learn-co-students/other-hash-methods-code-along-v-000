db84d01c12800d87c7d7e4fbf68a8156e4f90839
James Fernandez <jffernan@comcast.net>
2017-06-10 17:37:52 +0000

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -7,7 +7,15 @@ require 'pry'
 #  meat: ["chicken", "steak", "salmon"],
 #  grains: ["rice", "pasta"]
 # }
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
 
 def get_the_values(groceries)
   #code your solution here!
+  groceries.values.flatten
+  
 end
