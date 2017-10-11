66e44b1cf193d5ba2f5d8001b6708ecb77d52f84
Christopher DiStasio <cadistasio@gmail.com>
2017-10-11 03:10:49 +0000

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 #This is the groceries hash we'll be passing in to the get_the_values method
 # groceries = {
@@ -8,6 +8,15 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
+def get_the_keys(groceries)
+  groceries.keys
+end
+
+
+def get_the_min(groceries)
+  groceries.values.flatten.min
+end
+
 def get_the_values(groceries)
-  #code your solution here!
+  groceries.values.flatten
 end
