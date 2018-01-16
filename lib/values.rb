f6b8636bff0a228761ad34c988241bcec621cef0
Sean Clifford <sclifford6262@gmail.com>
2018-01-16 20:58:41 +0000

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -10,4 +10,12 @@ require 'pry'
 
 def get_the_values(groceries)
   #code your solution here!
+  val_array = []
+  groceries.values.each do |category|
+    category.each do |product|
+    val_array << product
+    end
+  end
+  #binding.pry
+  val_array
 end
