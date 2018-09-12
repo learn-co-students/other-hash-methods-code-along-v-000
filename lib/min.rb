369281fb4bf270d75deb35d2e59d26ff85db2332
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-12 17:35:22 +0000

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -9,5 +9,5 @@ require 'pry'
 # }
 
 def get_the_min(groceries)
-  groceries.min
+  groceries.values.flatten.min
 end
--- lib/values.rb
+++ lib/values.rb
@@ -9,5 +9,5 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  groceries.values
+  groceries.values.flatten
 end
